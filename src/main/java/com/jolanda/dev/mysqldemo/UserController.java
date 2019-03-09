7be3e5b10e6249119986f06963efac78dc7b6ad6
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2019-03-09 21:17:34 +0100

    two versions of getting the variables out of the mapping

--- src/main/java/com/jolanda/dev/mysqldemo/UserController.java
+++ src/main/java/com/jolanda/dev/mysqldemo/UserController.java
@@ -2,8 +2,10 @@ package com.jolanda.dev.mysqldemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserController {
@@ -12,17 +14,17 @@ public class UserController {
     private UserRepository repository;
 
     @GetMapping("/")
-    public String hello(){
+    public String hello() {
         return "Hello World";
     }
 
     @GetMapping("/users")
-    public Iterable<User> getAllUsers(){
+    public Iterable<User> getAllUsers() {
         return repository.findAll();
     }
 
     @GetMapping("/add")
-    public String add(){
+    public String add() {
 //        User newUser = new User();
 //        newUser.setName("Flo Rider");
 //        newUser.setEmail("flo@rider.de");
@@ -31,8 +33,27 @@ public class UserController {
         return "Nothing much happening here ";
     }
 
-    @PostMapping("/users/add/")
-    public String addUser(){
-        return "what you even trying to add?";
+    @GetMapping("/users/add")
+    public String addUser(HttpServletRequest request) {
+
+        // how would a request like this look?
+        // http://localhost:8080/users/add?name=ada
+        //User user = new User();
+        String name = request.getParameter("name");
+        return "what you even trying to add? This name? " + name;
+    }
+
+    @GetMapping("/users/adds")
+    public String anotherAdd(@RequestParam(value = "name", required = false) String name) {
+        // how would a request like this look?
+        // http://localhost:8080/users/adds?name=lovelace
+        return "add this name: " + name;
     }
 }
+
+
+/*
+@PathParam is a parameter annotation which allows you to map variable URI path fragments into your method call.
+
+@PathVariable is to obtain some placeholder from the URI (Spring call it an URI Template)
+ */
